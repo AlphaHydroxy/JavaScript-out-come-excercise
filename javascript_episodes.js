dbfa16c9efdc8b256858a1452f12d67f2f8cc62f
Jia Wong <jpw_4@msn.com>
2017-07-24 21:25:09 +0100

    inital commit

--- javascript_episodes.js
+++ javascript_episodes.js
@@ -1,6 +1,6 @@
 // 1.
 //code output = Hi! My name is Slim Shady
-// global variable name holds "Slim Shady" printName() function uses this global variable then function is called
+// global variable name holds "Slim Shady", printName() function uses this global variable through console.log which showw then function is called
 var name = 'Slim Shady';
 
 var printName = function() {
@@ -11,7 +11,7 @@ printName();
 
 // 2.
 // code output = 3
-// variable score is within the function which returns score, console calls the function this variable is stored
+// the variable score which holds 3 is within the result() function which returns score, console calls the result() function this variable is stored in
 score = 5;
 
 var result = function() {
@@ -23,7 +23,7 @@ console.log(result());
 
 // 3.
 // code output = ducks, dogs, lions
-// variable myAnimal within the listAnimals function takes precedence here despite var keyword being missing. It is within the function which console.logs calls
+// the variable myAnimal inside the listAnimals function would take precedence despite var keyword being missing. It is within the function which console.logs calls
 var myAnimals = ['chickens', 'cats', 'rabbits'];
 
 var listAnimals = function() {
@@ -36,8 +36,8 @@ var listAnimals = function() {
 listAnimals();
 
 // 4.
-// code output = Alan, Steve, Adam, Ally --- John
-// Adam takes precedence over John as variable suspectThree = ADAM is within the scope of allSuspects function when console.log is used. The console.log which is used outside the funtion cannot see the local variable which holds ADAM and should log JOHN
+// code output = Alan, Steve, Adam, Ally / John
+// Adam takes precedence over John as variable suspectThree = ADAM is within the scope of allSuspects function when console.log is used. The console.log which is used outside the function cannot see the local variable which holds ADAM and should log JOHN
 var suspectOne = 'Alan';
 var suspectTwo = 'Steve';
 var suspectThree = 'John';
@@ -52,8 +52,8 @@ allSuspects();
 console.log( 'Suspect three is:' + suspectThree );
 
 // 5.
-// code output = name: Poirot
-// console.log has detectiveInfo() function passed in, this function returns "Poirot"
+// code output = name: Ace Ventura pet: monkey
+// console.log has detectiveInfo() function passed in, this function returns hash variable detective
 var detective = {
   name : 'Ace Ventura',
   pet : 'monkey'
@@ -71,8 +71,8 @@ var detectiveInfo = function() {
 console.log(detectiveInfo());
 
 // 6.
-// code output = Ally and The murderer is John
-// Ally is shown when outFunctions is called. console.log has no access to any other variables apart from the global one which is John
+// code output = Steve / Ally / The murderer is John
+// Steve is shown when innerFunctions is called. Ally is shown when outerFunction is called, console.log has no access to any other variables apart from the global one which is John
 var murderer = 'John';
 
 var outerFunction = function() {
@@ -90,3 +90,9 @@ console.log('The murderer is', murderer);
 
 // 7.
 // Make up your own episode which can be whatever you wish and the rest of the class will work out together what happened and what the output will be.
+
+var goodBoy = "Rocky";
+var goodBoy = function(){
+
+}
+console.log(goodBoy + " is a good boy!!!");
